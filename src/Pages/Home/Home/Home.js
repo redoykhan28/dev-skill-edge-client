6b9fde44fed6ab0d960497896ced75d1a0122f10
unknown <redoyk2015@gmail.com>
2022-10-25 03:04:29 +0600

    router setup done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -4,7 +4,7 @@ import './Home.css'
 const Home = () => {
     return (
         <div>
-
+            <h4>Welcome home</h4>
         </div>
     );
 };
--- src/Route/Route.js
+++ src/Route/Route.js
@@ -1,6 +1,52 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home/Home"
+import Courses from "../Pages/Courses/Courses"
+import FAQ from "../Pages/FAQ/FAQ"
+import Blog from "../Pages/Blog/Blog"
+import Login from "../Pages/Login/Login"
+import SignUp from "../Pages/SignUp/SignUp"
 
 //setup route
 export const route = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
 
+            {
+                path: '/home',
+                element: <Home></Home>
+            },
+
+            {
+                path: '/courses',
+                element: <Courses></Courses>
+            },
+
+            {
+                path: '/faq',
+                element: <FAQ></FAQ>
+            },
+
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+
+            {
+                path: '/signup',
+                element: <SignUp></SignUp>
+            },
+        ]
+    }
 ])
