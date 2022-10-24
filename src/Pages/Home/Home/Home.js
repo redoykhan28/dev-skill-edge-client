3b33678dd89eca6d5cfcfb51f87cf1a42bf4b851
unknown <redoyk2015@gmail.com>
2022-10-25 05:03:42 +0600

    Hero section added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import headerImage from '../../../images/3785210.jpg'
 import './Home.css'
 
 const Home = () => {
     return (
         <div>
-            <h4 className='text-center'>Welcome home</h4>
-        </div>
+            {/* hero section start  */}
+            <section className='hero my-5'>
+                <div className="row">
+                    <div className="col-lg-5 col-md-6 heroHeading">
+                        <h1 className='m-color'>
+                            Every skill you acquire doubles your odds of
+                            <span> success...</span>
+                        </h1>
+                        <p className='my-3 text-muted'>
+                            Skill Edge is an E-lerning website where you can find multiple popular courses and learning stuff which can improve your skill and learning ability beyond expectation...
+                        </p>
+
+                        <a href='#courses'><button className='btn startBtn'>Get Started</button></a>
+                    </div>
+                    <div className="col-lg-7 col-md-6">
+                        <img className='img-fluid heroImg' src={headerImage} alt="img" />
+                    </div>
+                </div>
+            </section >
+            {/* hero section end  */}
+
+            <section className='courses' id='courses'>
+                <h3 className='text-center'>Explore Our leatest courses</h3>
+            </section>
+
+
+        </div >
     );
 };
 
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import logo from '../../../images/d0d29493594148fd9a0e3c13fdc85c4c (3).png'
 import './Header.css'
-import { FaToggleOff } from "react-icons/fa";
+import { FaToggleOff, FaSun } from "react-icons/fa";
 
 const Header = () => {
     return (
@@ -22,14 +22,20 @@ const Header = () => {
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <NavLink className={'text-muted mx-3 pt-1 text-decoration-none'} to='/home'>Home</NavLink>
-                                <NavLink className={'text-muted mx-3 pt-1 text-decoration-none'} to='/courses'>Courses</NavLink>
-                                <NavLink className={'text-muted mx-3 pt-1 text-decoration-none'} to='/faq'>FAQ</NavLink>
-                                <NavLink className={'text-muted mx-3 pt-1 text-decoration-none'} to='/blog'>Blog</NavLink>
+
+                                <NavLink className={({ isActive }) => isActive ? 'home text-light py-1 py-lg-2 px-3' : 'text-muted mx-3 pt-2 text-decoration-none'} to='/home'>Home</NavLink>
+
+                                <NavLink className={({ isActive }) => isActive ? 'home text-light py-1 py-lg-2 px-3' : 'text-muted mx-3 pt-2 text-decoration-none'} to='/courses'>Courses</NavLink>
+
+                                <NavLink className={({ isActive }) => isActive ? 'home text-light py-1 py-lg-2 px-3' : 'text-muted mx-3 pt-2 text-decoration-none'} to='/faq'>FAQ</NavLink>
+
+                                <NavLink className={({ isActive }) => isActive ? 'home text-light py-1 py-lg-2 px-3' : 'text-muted mx-3 pt-2 text-decoration-none'} to='/blog'>Blog</NavLink>
+
                                 <NavLink className={'btn s-btn px-4 mt-3 mt-lg-0 ms-lg-5'} to='/login'>Login</NavLink>
+
                                 <div className='d-flex align-items-center'>
-                                    <NavLink className={' ms-3 text-decoration-none'} to='/courses'><FaToggleOff className='togcolor pt-1' /></NavLink>
-                                    <small className=' mx-1 fw-bold'>Light</small>
+                                    <NavLink className={' ms-3 text-decoration-none'} to='/courses'><FaToggleOff className='togcolor' /></NavLink>
+                                    <small className=' mx-2 fw-bold'>Light <FaSun className='fs-4'></FaSun></small>
                                 </div>
                             </ul>
                         </div>
