4012fb969db3ded733d3152e4d15c8cb567c11fb
unknown <redoyk2015@gmail.com>
2022-10-26 04:43:32 +0600

    checkout added

--- src/Pages/Courses/CheckOut/Checkout.js
+++ src/Pages/Courses/CheckOut/Checkout.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { FaShoppingBag } from "react-icons/fa";
+import toast, { Toaster } from 'react-hot-toast';
+import './CheckOut.css'
 
 const Checkout = () => {
     const check = useLoaderData();
-    console.log(check)
+    // console.log(check)
+    const { title, price } = check
+
+    const purchaseHandler = () => {
+
+        toast.success(`${title} Purchase Successfully`)
+
+    }
+
     return (
         <div>
-
+            <div className="card checkCard ">
+                <div className="card-body">
+                    <h3 className='text-center my-3'>Checkout</h3>
+                    <div className='d-flex justify-content-between mt-5'>
+                        <h6>1. Course: {title}</h6>
+                        <h6>Price: {price}</h6>
+                    </div>
+                    <hr />
+                    <div className='d-flex justify-content-between'>
+                        <h5>Total:</h5>
+                        <div>
+                            <h5>{price}</h5>
+                        </div>
+                    </div>
+                    <div className='d-flex justify-content-between mt-4'>
+                        <select class="form-select selector border-0" aria-label="Default select example">
+                            <option selected>Select payment Method</option>
+                            <option value="1">Bkash</option>
+                            <option value="2">Paypal</option>
+                            <option value="3">Credit card</option>
+                        </select>
+                        <button onClick={purchaseHandler} className='btn purchase-btn my-4'>Purchase <FaShoppingBag /></button>
+                    </div>
+                </div>
+            </div>
+            <Toaster />
         </div>
     );
 };
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,9 +3,26 @@ import './Login.css'
 
 const Login = () => {
     return (
-        <div>
+        <div className='container'>
+            <div className="card">
 
+                <form>
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" className="form-label input">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleInputPassword1" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                    </div>
+
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+
+            </div>
         </div>
+
     );
 };
 
